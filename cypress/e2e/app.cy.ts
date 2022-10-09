023ed0d5c52767ab4ebf94c3cb5561da34339ccc
chca <cachet.cha@gmail.com>
2022-10-10 00:31:54 +0200

    fix ci ansd prettier

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,11 +1,11 @@
-describe('Navigation', () => {
-	it('should navigate to the about page', () => {
-		// Start from the index page
-		cy.visit('http://localhost:3000/');
+describe("Navigation", () => {
+  it("should navigate to the about page", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000/");
 
-		// The new page should contain an h1 with "About page"
-		cy.get('h1').contains('Welcome');
-	});
+    // The new page should contain an h1 with "About page"
+    cy.get("h1").contains("Welcome");
+  });
 });
 
 // Prevent TypeScript from reading file as legacy script
